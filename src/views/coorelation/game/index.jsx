0ea58facfffcefefdc323e54b4ac9cce3e617874
aqmeraamir <aqmeraamir@gmail.com>
2025-08-23 21:10:51 +0400

    update

--- src/views/coorelation/game/index.jsx
+++ src/views/coorelation/game/index.jsx
@@ -3,13 +3,17 @@ import GameChart from "views/coorelation/game/components/GameChart";
 const Game = () => {
   return (
     <div>
-        <div className="mt-1 text-center text-sm text-gray-500">
-          Credit to <a href="https://i-am.cool/maths/game.html" className="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">i-am.cool/maths</a>
-        </div>
+        
 
         {/* Game */}
         <div className="mt-0 grid grid-cols-1 min-h-[500px] max-w-[2500px]">
           <GameChart />
+        </div>
+
+        <br></br>
+
+        <div className="mt-1 text-center text-sm text-gray-500">
+          Credit to <a href="https://i-am.cool/maths/game.html" className="text-blue-500 hover:underline" target="_blank" rel="noopener noreferrer">i-am.cool/maths</a>
         </div>
     </div>
   );
